338d1f980f909fa28fa11f8fd5343394634230e2
Manuel Ernesto <alemdadox@gmail.com>
2021-09-15 21:15:44 +0100

    #email service init

---
+++ email-service/src/test/java/com/adidas/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adidas.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
