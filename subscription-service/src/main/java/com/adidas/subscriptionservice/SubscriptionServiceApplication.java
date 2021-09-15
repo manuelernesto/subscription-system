2ff4155adbfe9c1548257d3bd51226d90acac7c3
Manuel Ernesto <alemdadox@gmail.com>
2021-09-15 22:10:45 +0100

    #subscription service init

---
+++ subscription-service/src/main/java/com/adidas/subscriptionservice/SubscriptionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.adidas.subscriptionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubscriptionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubscriptionServiceApplication.class, args);
+    }
+
+}
---
+++ subscription-service/src/test/java/com/adidas/subscriptionservice/SubscriptionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adidas.subscriptionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SubscriptionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
